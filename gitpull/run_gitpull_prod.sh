b0eaf1bf85b7698a17f1ff8e80beff24ef2c4dcc
Chay Harley <chayharley@noreply.com>
2025-04-10 23:52:38 +1000

    git_pull workflow for import

--- gitpull/run_gitpull_prod.sh
+++ gitpull/run_gitpull_prod.sh
@@ -3,6 +3,6 @@
 # overriding the values for the prod environment
 export SITUATE_URL="https://situate-production/situate"
 export SITUATE_APIKEY="aKLPo9XaCbRTz93Yq5DaYXNaZtPaJmNantBa8CWbZQ"
-export WORKFLOW_ID="8a2f9c5a-4c7e-11eb-bc3a-0242ac110002"
+export WORKFLOW_ID="9n524db4-1611-11f0-8b06-475160e3b1a3"
 
 ./run_gitpull.sh
